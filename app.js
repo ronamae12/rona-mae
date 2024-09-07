558c2656cac63a508e7616c6135bb3a5a3b931aa
Rona Mae Ancheta <ronamaeancheta18@gmail.com>
2024-09-07 21:04:45 +0800

    Completed Variables Section

--- app.js
+++ app.js
@@ -1,3 +1,34 @@
-console.log('Hello, World');
-let name = "Rona Mae A. Ancheta";
-console.log('My name is' + name);
+console.log('Hello, World!');
+let myname = "Rona Mae Ancheta";
+console.log('My name is ' + myname);
+
+console.log('USING VARIABLES');
+let name = "Rona Mae Ancheta";
+let age = "19";
+console.log("Hello! I am " + name + ", " + age + " years old. ");
+
+console.log("USING NUMBERS");
+const a =  19
+const b = 20
+
+const sum = x + y;
+console.log(sum);
+
+console.log('USING BOOLEANS');
+let c = true;
+let d = false;
+console.log('I am learning programming', c);
+console.log('I will give up', d);
+
+console.log('COMPARISON OPERATORS SECTION');
+let e = 5
+let f = 10
+
+let g = e > f;
+console.log("Is e more than f? "+ g);
+let h = e < f;
+console.log("Is e less than f? "+ h);
+let i = e === f;
+console.log("Is e equal to f? "+ i);
+let j = e != f;
+console.log("Is e not equal to f? "+ j);
